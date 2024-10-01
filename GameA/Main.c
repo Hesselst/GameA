3ff4137c8f240128850943b7fff7d9dbfcf40b98
Hesselst <hesselsteenwoerd@gmail.com>
2024-10-01 19:56:00 +0200

    Initial commit

---
+++ GameA/Main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#pragma warning(push, 0)
+#pragma warning(disable:4668)
+#pragma warning(disable:5105)
+#include <windows.h>
+#pragma warning(pop)
+
+int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previousInstance, _In_ LPSTR cmdLine, _In_ int showCmd)
+{
+    UNREFERENCED_PARAMETER(instance);
+
+    UNREFERENCED_PARAMETER(previousInstance);
+
+    UNREFERENCED_PARAMETER(cmdLine);
+
+    UNREFERENCED_PARAMETER(showCmd);
+
+    return 0;
+}
