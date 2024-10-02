f1aa6bab08a9d5ba242316120d54322f65dc188f
Hesselst <hesselsteenwoerd@gmail.com>
2024-10-02 20:07:43 +0200

    changed header file

--- GameA/Main.h
+++ GameA/Main.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#define GAME_NAME "Game_A"
+
+LRESULT CALLBACK MainWindowProc(_In_ HWND windowHandle, _In_ UINT message, _In_ WPARAM wParam, _In_ LPARAM lParam
+);
+
+DWORD CreateMainGameWindow(void);
+
+BOOL GameIsAlreadyRunning(void);
